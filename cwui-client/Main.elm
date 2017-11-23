cf72a8dcfc750930c85073faef3dd8f912f75746
David Honour <david@concertdaw.co.uk>
2017-11-23 10:05:05 +0000

    More realistic types

--- cwui-client/Main.elm
+++ cwui-client/Main.elm
@@ -1,4 +1,5 @@
 import Dict exposing (..)
+import Set exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
@@ -9,13 +10,43 @@ main = Html.program {
 
 -- Model
 
-type alias ClType = String
-type alias ClValue = String
-type alias ClPath = String
-type alias ClTime = Int
+type alias Path = String
+type alias ChildName = String
+type alias Time = Int
+type alias Attributee = String
+type alias Site = String
 
-type alias ClSeries = Dict ClTime ClValue
+type Liberty
+  = Cannot
+  | May
+  | Must
 
+type Interpolation
+  = IConstant
+  | ILinear
+
+type alias ClAtomType = String
+
+type ClType
+  = ClTuple {doc : String, names : List String, atomTypes : List ClAtomType, interpolations : Set Interpolation}
+  | ClStruct {doc : String, childNames: List ChildName, childTypes : List Path, childLiberties : List Liberty}
+  | ClArray {doc : String, childType : Path, childLiberty : Liberty}
+
+type ClValue
+  = ClTime Time
+  | ClEnum Int
+  | ClWord32 Int
+  | ClWord64 Int
+  | ClInt32 Int
+  | ClInt64 Int
+  | ClFloat Float
+  | ClDouble Float
+  | ClString String
+  | ClList (List ClValue)
+
+type alias ClSeries = Dict Time (List ClValue)
+
+-- FIXME: nowhere to put tree structure or errors!
 type alias ClNode =
   { typeInfo : Maybe ClType
   , values : ClSeries
@@ -25,30 +56,22 @@ type alias ClNode =
 emptyNode : ClNode
 emptyNode = ClNode Nothing Dict.empty Dict.empty
 
-type alias NodeMap = Dict ClPath ClNode
+type alias TypeMap = Dict Path Path
+type alias NodeMap = Dict Path ClNode
 
+-- FIXME: Drops site and attribution
 type alias Model =
-  { nodes : NodeMap
+  { types : TypeMap
+  , nodes : NodeMap
   , partialEntry : String
   }
 
 init : (Model, Cmd Msg)
-init = (Model (Dict.singleton "lemon" emptyNode) "somestring", Cmd.none)
+init = (Model Dict.empty Dict.empty "somestring", Cmd.none)
 
 -- Update
 
 type InterfaceEvent = UpPartial String | IfAdd String | IfDrop String
-type NetworkEvent = NeAdd String | NeDrop String
-
-type Msg = InterfaceEvent InterfaceEvent | NetworkEvent (List NetworkEvent)
-
-networkUpdate1 : NetworkEvent -> NodeMap -> NodeMap
-networkUpdate1 ne nodes = case ne of
-    (NeAdd s) -> Dict.insert s emptyNode nodes
-    (NeDrop s) -> Dict.remove s nodes
-
-networkUpdate : List NetworkEvent -> NodeMap -> (NodeMap, Cmd Msg)
-networkUpdate nes nodes = (List.foldl networkUpdate1 nodes nes, Cmd.none)
 
 interfaceUpdate : InterfaceEvent -> Model -> (Model, Cmd Msg)
 interfaceUpdate ie model = case ie of
@@ -56,13 +79,98 @@ interfaceUpdate ie model = case ie of
     (IfAdd s) -> (model, WebSocket.send "ws://echo.websocket.org" (String.cons 'a' s))
     (IfDrop s) -> (model, WebSocket.send "ws://echo.websocket.org" (String.cons 'd' s))
 
+type ErrorMsg = ErrorMsg Path String
+
+type TreeUpdateMsg
+  = MsgAssignType Path Path
+  | MsgDelete Path
+
+handleTreeUpdateMsg : TreeUpdateMsg -> TypeMap -> TypeMap
+handleTreeUpdateMsg tum tm = case tum of
+    (MsgAssignType p tp) -> Dict.insert p tp tm
+    (MsgDelete p) -> Dict.remove p tm
+
+type DataUpdateMsg
+  = MsgAdd
+      { msgPath : Path
+      , msgTime : Time
+      , msgArgs : (List ClValue)
+      , msgInterpolation : Interpolation
+      , msgAttributee : (Maybe Attributee)
+      , msgSite : (Maybe Site)
+      }
+  | MsgSet
+      { msgPath : Path
+      , msgTime : Time
+      , msgArgs : List ClValue
+      , msgInterpolation : Interpolation
+      , msgAttributee : Maybe Attributee
+      , msgSite : Maybe Site
+      }
+  | MsgRemove
+      { msgPath : Path
+      , msgTime : Time
+      , msgAttributee : Maybe Attributee
+      , msgSite : Maybe Site
+      }
+  | MsgClear
+      { msgPath : Path
+      , msgTime : Time
+      , msgAttributee : Maybe Attributee
+      , msgSite : Maybe Site
+      }
+  | MsgSetChildren
+      { msgPath : Path
+      , msgChildren : List ChildName
+      , msgAttributee : Maybe Attributee
+      }
+
+handleDataUpdateMsg : DataUpdateMsg -> ClNode -> ClNode
+handleDataUpdateMsg dum n = let nv = .values n in case dum of
+    (MsgAdd {msgTime, msgArgs}) -> {n | values = Dict.insert msgTime msgArgs nv}
+    (MsgSet {msgTime, msgArgs}) -> {n | values = Dict.insert msgTime msgArgs nv}
+    (MsgRemove {msgTime}) -> {n | values = Dict.remove msgTime nv}
+    (MsgClear {}) -> n  -- FIXME: does nothing because single site
+    (MsgSetChildren {}) -> n  -- FIXME: don't have anywhere to put this in the model ATM
+
+type UpdateMsg = TreeUpdateMsg TreeUpdateMsg | DataUpdateMsg DataUpdateMsg
+
+updateNode : (ClNode -> ClNode) -> Path -> NodeMap -> NodeMap
+updateNode t p m =
+  let
+    wt mn = Just (t (Maybe.withDefault emptyNode mn))
+  in
+    Dict.update p wt m
+
+dumPath : DataUpdateMsg -> Path
+dumPath dum = case dum of
+    (MsgAdd {msgPath}) -> msgPath
+    (MsgSet {msgPath}) -> msgPath
+    (MsgRemove {msgPath}) -> msgPath
+    (MsgClear {msgPath}) -> msgPath
+    (MsgSetChildren {msgPath}) -> msgPath
+
+handleUpdateMsg : UpdateMsg -> (NodeMap, TypeMap) -> (NodeMap, TypeMap)
+handleUpdateMsg um (nodes, types) = case um of
+    (TreeUpdateMsg tum) -> (nodes, handleTreeUpdateMsg tum types)
+    (DataUpdateMsg dum) -> (updateNode (handleDataUpdateMsg dum) (dumPath dum) nodes, types)
+
+type UpdateBundle = UpdateBundle (List ErrorMsg) (List UpdateMsg)
+
+handleUpdateBundle : UpdateBundle -> (NodeMap, TypeMap) -> (NodeMap, TypeMap)
+handleUpdateBundle (UpdateBundle errs updates) maps =
+    List.foldl handleUpdateMsg maps updates
+    -- FIXME: Ignores errors and doesn't sort types
+
+type Msg = InterfaceEvent InterfaceEvent | NetworkEvent UpdateBundle
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
-    (NetworkEvent ne) ->
+    (NetworkEvent b) ->
       let
-        (newNodes, cmd) = networkUpdate ne (.nodes model)
+        (newNodes, newTypes) = handleUpdateBundle b (.nodes model, .types model)
       in
-        ({model | nodes = newNodes}, cmd)
+        ({model | nodes = newNodes, types = newTypes}, Cmd.none)
     (InterfaceEvent ie) -> interfaceUpdate ie model
 
 -- Subscriptions
@@ -70,13 +178,10 @@ update msg model = case msg of
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.map NetworkEvent (WebSocket.listen "ws://echo.websocket.org" eventFromNetwork)
 
-eventFromNetwork : String -> List NetworkEvent
-eventFromNetwork s = case String.uncons s of
-    (Just (h, t)) -> case h of
-        'a' -> [NeAdd t]
-        'd' -> [NeDrop t]
-        _ -> []
-    Nothing -> []
+eventFromNetwork : String -> UpdateBundle
+eventFromNetwork s = UpdateBundle [] [DataUpdateMsg (MsgAdd {
+    msgPath = "/someplace", msgTime = 0, msgArgs = [], msgInterpolation = IConstant,
+    msgAttributee = Nothing, msgSite = Nothing})]
 
 -- View
 
