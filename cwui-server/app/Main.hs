c8efc498615d5b9696b402f48cc839006df829a6
David Honour <david@concertdaw.co.uk>
2017-11-27 13:04:00 +0000

    Make an overall protocol

--- cwui-server/app/Main.hs
+++ cwui-server/app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
+import Data.ByteString (ByteString)
+
+import Clapi.Protocol (Protocol, (<<->))
+import Clapi.SerialisationProtocol (serialiser)
+import JsonProto (jsonProto)
+
+formatConvProto ::
+    Monad m => Protocol
+        ByteString
+        ByteString
+        ByteString
+        ByteString
+        m ()
+formatConvProto = serialiser <<-> jsonProto
 
 main :: IO ()
 main = do
