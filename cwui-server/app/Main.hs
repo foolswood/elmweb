d8c360d3f562b9a4e23235433c05c9d451b5e948
David Honour <david@concertdaw.co.uk>
2017-11-27 13:27:21 +0000

    Make a crude converting bridge out of the bits

--- cwui-server/app/Main.hs
+++ cwui-server/app/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 import Data.ByteString (ByteString)
+import Data.Maybe (fromJust)
+import Control.Monad (void)
+import qualified Network.Simple.TCP as TCP
+import qualified Network.WebSockets as WS
 
-import Clapi.Protocol (Protocol, (<<->))
+import Clapi.Protocol (Protocol, (<<->), runProtocolIO)
 import Clapi.SerialisationProtocol (serialiser)
 import JsonProto (jsonProto)
 
@@ -14,6 +18,19 @@ formatConvProto ::
         m ()
 formatConvProto = serialiser <<-> jsonProto
 
+bridge :: WS.Connection -> (TCP.Socket, TCP.SockAddr) -> IO ()
+bridge ws (s, _) = runProtocolIO
+    (fromJust <$> TCP.recv s 1024)
+    (WS.sendTextData ws)
+    (void . TCP.send s)
+    (WS.receiveData ws)
+    formatConvProto
+
+connectToRelay :: WS.Connection -> IO ()
+connectToRelay ws = TCP.connect "127.0.0.1" "1234" (bridge ws)
+
+acceptWs :: WS.PendingConnection -> IO ()
+acceptWs p = WS.acceptRequest p >>= connectToRelay
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = WS.runServer "127.0.0.1" 8004 acceptWs
