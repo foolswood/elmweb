e1bcbbfd4f285428284d268bcc2258d296bcf3b3
Grahame Gardiner <grahame@concertdaw.co.uk>
2019-04-09 15:25:28 +0100

    Log prettier json

--- cwui-server/src/JsonProto.hs
+++ cwui-server/src/JsonProto.hs
@@ -3,6 +3,8 @@ module JsonProto (jsonProto) where
 import Data.Aeson (encode, eitherDecode)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as LB
+import Data.Text.Encoding (decodeUtf8)
+import Data.Text (unpack)
 
 import Control.Monad (forever)
 import Clapi.Types (SomeFrDigest, SomeTrDigest, TimeStamped)
@@ -11,8 +13,8 @@ import JsonConv ()
 
 import Debug.Trace
 
-tt :: Show a => String -> a -> a
-tt tag a = trace (tag ++ ": " ++ show a) a
+tt :: String -> B.ByteString -> B.ByteString
+tt tag a = trace (tag ++ ": " ++ unpack (decodeUtf8 a)) a
 
 jsonProto ::
     Monad m => Protocol
@@ -25,5 +27,5 @@ jsonProto = forever $ waitThen jsonMash jsonUnmash
   where
     jsonMash b = sendFwd $ tt "->" $ LB.toStrict $ encode b
     jsonUnmash bs = case eitherDecode $ LB.fromStrict $ tt "<-" bs of
-        Left s -> error $ "Decode failure: " ++ (show bs) ++ " - " ++ s
+        Left s -> error $ "Decode failure: " ++ show bs ++ " - " ++ s
         Right b -> sendRev b
